20b63af0d7725175e89f1bb6e99275a19b7d8951
Moises Pahati <moisespahati@gmail.com>
2023-07-16 18:40:50 +0800

    add initialItems array of objects

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+
 export default function App() {
   return (
     <div className="app">
@@ -20,8 +26,6 @@ function Form() {
     </div>
   )
 }
-
-
 
 function PackingList() {
   return (

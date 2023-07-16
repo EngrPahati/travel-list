400fcb4beeda872216a3895f35641111abbb0eed
Moises Pahati <moisespahati@gmail.com>
2023-07-16 20:42:51 +0800

    render list of number <options>

--- src/App.js
+++ src/App.js
@@ -25,7 +25,8 @@ function Form() {
     <form className="add-form">
       <h3>What do you need for your 😍 trip?</h3>
       <select>
-        {Array.from({length: 20}, (_, i) => i + 1)}
+        {Array.from({ length: 20 }, (_, i) => i + 1)
+          .map(num => <option value={num} key={num}>{ num }</option>)}
       </select>
       <input type="text" placeholder="Item..." />
       <button>Add</button>

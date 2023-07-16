bcec6ff9bbd08d326be19c5c27ca56136d81f5f5
Moises Pahati <moisespahati@gmail.com>
2023-07-16 18:38:44 +0800

    update footer styles

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function PackingList() {
 
 function Stats() { 
   return (
-    <footer>
+    <footer className="stats">
       <em>You have X items on your list, and you already packed X (X%)</em>
     </footer>
   )

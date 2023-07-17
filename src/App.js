0bfd1da919c1c48f0db1ef777b51ab1145246089
Moises Pahati <moisespahati@gmail.com>
2023-07-17 23:32:55 +0800

    implement sorting features by input, description and packed

--- src/App.js
+++ src/App.js
@@ -89,10 +89,25 @@ function Form({ onAddItems }) {
 function PackingList({ items, onDeleteItem, onToggleItem }) {
   const [sortBy, setSortBy] = useState("input");
 
+  let sortedItems;
+  
+  if (sortBy === 'input') sortedItems = items;
+
+  if (sortBy === 'description') {
+    sortedItems = items
+    .slice()
+    .sort((a, b) => a.description.localeCompare(b.description))
+  }
+
+  if (sortBy === 'packed') sortedItems = items
+    .slice()
+    .sort((a, b) => Number(b.packed) - Number(a.packed))
+  
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}

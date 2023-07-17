b0fc6597fe90ffbf271fd42d5b1e90a27f48cc78
Moises Pahati <moisespahati@gmail.com>
2023-07-17 23:09:23 +0800

    update Stats component

--- src/App.js
+++ src/App.js
@@ -121,9 +121,17 @@ function Item({ item, onDeleteItem, onToggleItem}) {
 
 function Stats({ items }) { 
   const numItems = items.length;
+  const numPacked = items.filter(item => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100) || "0";
+
   return (
     <footer className="stats">
-      <em>You have {numItems} items on your list, and you already packed X (X%)</em>
+      <em>
+      {percentage === 100 ? 'You got everything! Ready to GO!✈' : 
+        `You have ${numItems} items on your list, and you already packed ${ numPacked } (${ percentage }%)`
+        }
+      </em>
+
     </footer>
   )
 }

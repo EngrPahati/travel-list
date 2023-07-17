355c606b32546062cad4168418478288a9a9124b
Moises Pahati <moisespahati@gmail.com>
2023-07-17 12:44:24 +0800

    use lifting state up to share state between sibling components

--- src/App.js
+++ src/App.js
@@ -8,11 +8,17 @@ const initialItems = [
 
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems(items => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={ handleAddItems } />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -22,9 +28,11 @@ function Logo() {
   return <h1>🌴Far Away👜</h1>
  }
 
-function Form() { 
+function Form({ onAddItems }) { 
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
+
 
 
   function handleSubmit(e) {
@@ -34,6 +42,8 @@ function Form() {
 
     const newItem = { description, quantity, packed: false, id: Date.now() }
     console.log(newItem);
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -59,11 +69,11 @@ function Form() {
   )
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={ item } key={ item.id } />
         ))}
       </ul>

ea4719b7d0acf47150831cda2627d0791a45bad7
Moises Pahati <moisespahati@gmail.com>
2023-07-17 21:51:45 +0800

    fix bugs on onToggleItem function of Item component

--- src/App.js
+++ src/App.js
@@ -106,7 +106,10 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
 function Item({ item, onDeleteItem, onToggleItem}) {
   return (
     <li>
-      <input type='checkbox' value={ item.packed} onChange={}/>
+      <input
+        type='checkbox'
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)} />
       <span style={ item.packed? {textDecoration: "line-through"} : {}}>{item.quantity} {item.description}</span>
       <button
         onClick={() => onDeleteItem(item.id)}
